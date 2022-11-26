1df1126a0421bcc3528a75af1e7ed5d09deac16b
David Sosby <dsosby@gmail.com>
2022-11-26 10:28:58 -0800

    Parse day args

--- cmd/root.go
+++ cmd/root.go
@@ -5,8 +5,11 @@ Copyright © 2022 David Sosby <dsosby@gmail.com>
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -15,8 +18,31 @@ var rootCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run a days test",
 	Long:  `Run a days test`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		switch len(args) {
+		case 0:
+			return nil
+		case 1:
+			_, err := strconv.Atoi(args[0])
+			return err
+		default:
+			return errors.New("Too many day args")
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Want me to run the latest day?")
+		day := 1
+
+		if len(args) == 1 {
+			argday, err := strconv.Atoi(args[0])
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			day = argday
+		}
+
+		fmt.Println("Running day", day)
 	},
 }
 
