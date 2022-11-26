7f8d1edcf0c025d6ec6e6378c605129552c6ad16
David Sosby <dsosby@gmail.com>
2022-11-26 10:42:56 -0800

    Utils package

---
+++ cmd/newday.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2022 David Sosby <dsosby@gmail.com>
+
+*/
+package cmd
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/dsosby/aoc2022/pkg"
+	"github.com/spf13/cobra"
+)
+
+// newdayCmd represents the newday command
+var newdayCmd = &cobra.Command{
+	Use:   "newday",
+	Short: "Initializes a new day package",
+  Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+    day, err := strconv.Atoi(args[0])
+
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    util.CreateDayPackage(day)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(newdayCmd)
+}
---
+++ pkg/util.go
@@ -0,0 +1,7 @@
+package util
+
+import "fmt"
+
+func CreateDayPackage(day int) {
+	fmt.Println("Creating a new day for ", day)
+}
