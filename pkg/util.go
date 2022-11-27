4dd8015a62f4bddffb6902b362f3dde06d091e38
David Sosby <david@sosby.org>
2022-11-26 16:46:40 -0800

    Let's try templating maybe

--- pkg/util.go
+++ pkg/util.go
@@ -1,7 +1,43 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+  "text/template"
+)
+
+type TemplateVars struct {}
+
+func fileExists(name string) bool {
+  _, err := os.Stat(name)
+  return err == nil
+}
 
 func CreateDayPackage(day int) {
-	fmt.Println("Creating a new day for ", day)
+  packageName := fmt.Sprintf("day%02d", day)
+  srcName := filepath.Join(packageName, "day.go")
+
+  if !fileExists(packageName) {
+    mkdirErr := os.Mkdir(packageName, 0755)
+    if mkdirErr != nil {
+      panic(mkdirErr)
+    }
+  }
+
+  if !fileExists(srcName) {
+    t, templateErr := template.ParseFiles("template.go.tmpl")
+    if templateErr != nil {
+      panic(templateErr)
+    }
+
+    f, fileErr := os.Create(srcName)
+    defer f.Close()
+    if fileErr != nil {
+      panic(fileErr)
+    }
+
+    vars := TemplateVars{}
+    t.Execute(f, vars)
+  }
 }
