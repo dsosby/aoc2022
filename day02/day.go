a6bfa6c9fa99bb02a17ac7c2c4564a360a38b07c
David Sosby <david@sosby.org>
2022-12-01 21:22:33 -0800

    Problem 1

---
+++ day02/day.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	usePart2 := len(os.Args) > 1 && os.Args[1] == "--second"
+	if usePart2 {
+		Problem2()
+	} else {
+		Problem1()
+	}
+}
+
+func Problem1() {
+	entries := readEntries()
+  total := 0
+
+  for _, play := range entries {
+    total += play.getScore()
+  }
+
+  fmt.Println(total)
+}
+
+func Problem2() {
+	fmt.Println("Solution Two")
+}
+
+type Play struct {
+	them byte
+	you  byte
+}
+
+func shapeScore(you rune) int {
+	switch you {
+	case 'X':
+		return 1
+	case 'Y':
+		return 2
+	case 'Z':
+		return 3
+	}
+
+	return 0
+}
+
+// a, x == rock
+// b, y == paper
+// c, z == scissors
+
+func (p *Play) outcomeScore() int {
+	if (p.you == 'X' && p.them == 'C') ||
+		(p.you == 'Y' && p.them == 'A') ||
+		(p.you == 'Z' && p.them == 'B') {
+		return 6 // Win
+	}
+
+	if (p.you == 'X' && p.them == 'B') ||
+		(p.you == 'Y' && p.them == 'C') ||
+		(p.you == 'Z' && p.them == 'A') {
+		return 0 // Lose
+	}
+
+	return 3 // Draw
+}
+
+func (p *Play) getScore() int {
+	shapeScore := shapeScore(rune(p.you))
+	winScore := p.outcomeScore()
+	return shapeScore + winScore
+}
+
+func readEntries() []Play {
+	file, fileErr := os.Open("day02/input.txt")
+	if fileErr != nil {
+		panic(fileErr)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	entries := make([]Play, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		entry := Play{
+			them: line[0],
+			you:  line[2],
+		}
+
+		entries = append(entries, entry)
+	}
+
+	return entries
+}
---
+++ day02/thoughts.md
@@ -0,0 +1,1 @@
+Go error messages are ... bad
