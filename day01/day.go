d9eb3cb9f79ea7c35715a31e39bfe4896118ab1c
David Sosby <david@sosby.org>
2022-12-01 20:43:11 -0800

    Problem 2 -- hacks

--- day01/day.go
+++ day01/day.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -14,6 +15,38 @@ func main() {
   } else {
     Problem1()
   }
+}
+
+func readCalories() []int {
+  file, fileErr := os.Open("day01/input.txt")
+  if fileErr != nil {
+    panic(fileErr)
+  }
+
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+
+  calories := make([]int, 10)
+  var cur int = 0
+
+  for scanner.Scan() {
+    line := scanner.Text()
+
+    if line == "" {
+      calories = append(calories, cur)
+      cur = 0
+    } else {
+      num, err := strconv.Atoi(line)
+      if err != nil {
+        panic(err)
+      }
+
+      cur += num
+    }
+  }
+
+  return calories
 }
 
 func Problem1() {
@@ -52,5 +85,16 @@ func Problem1() {
 }
 
 func Problem2() {
-  fmt.Println("Solution Two")
+  calories := readCalories()
+  sort.Slice(calories, func(i, j int) bool {
+    return calories[i] > calories[j]
+  })
+  
+  top := calories[:3]
+  sum := 0
+  for _, cal := range top {
+    sum += cal
+  }
+
+  fmt.Println(sum)
 }
