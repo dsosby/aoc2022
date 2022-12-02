f6b7ddfe54b9da49a972e85341727b82c87242a6
David Sosby <david@sosby.org>
2022-12-01 20:56:56 -0800

    Reuse

--- day01/day.go
+++ day01/day.go
@@ -17,6 +17,35 @@ func main() {
   }
 }
 
+func Problem1() {
+  calories := readCalories()
+
+  max := 0
+  for _, calorie := range calories {
+    if (calorie > max) {
+      max = calorie
+    }
+  }
+
+  fmt.Printf("%d\n", max)
+}
+
+func Problem2() {
+  calories := readCalories()
+  sort.Slice(calories, func(i, j int) bool {
+    return calories[i] > calories[j]
+  })
+  
+  top := calories[:3]
+  sum := 0
+  for _, cal := range top {
+    sum += cal
+  }
+
+  fmt.Println(sum)
+}
+
+// Probably should return err, but meh
 func readCalories() []int {
   file, fileErr := os.Open("day01/input.txt")
   if fileErr != nil {
@@ -47,54 +76,4 @@ func readCalories() []int {
   }
 
   return calories
-}
-
-func Problem1() {
-  file, fileErr := os.Open("day01/input.txt")
-  if fileErr != nil {
-    panic(fileErr)
-  }
-
-  defer file.Close()
-
-  scanner := bufio.NewScanner(file)
-
-  max := 0
-  cur := 0
-
-  for scanner.Scan() {
-    line := scanner.Text()
-
-    if line == "" {
-      if cur > max {
-        max = cur
-      }
-
-      cur = 0
-    } else {
-      num, err := strconv.Atoi(line)
-      if err != nil {
-        panic(err)
-      }
-
-      cur += num
-    }
-  }
-
-  fmt.Printf("%d\n", max)
-}
-
-func Problem2() {
-  calories := readCalories()
-  sort.Slice(calories, func(i, j int) bool {
-    return calories[i] > calories[j]
-  })
-  
-  top := calories[:3]
-  sum := 0
-  for _, cal := range top {
-    sum += cal
-  }
-
-  fmt.Println(sum)
 }
